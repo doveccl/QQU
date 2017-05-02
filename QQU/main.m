434b0538fce57c21e4d73df10e15316e238f09b3
Doveccl <doveccl@live.com>
2017-05-02 12:53:55 +0800

    update

---
+++ QQU/main.m
@@ -0,0 +1,30 @@
+#import <stdio.h>
+#import <stdlib.h>
+#import <string.h>
+
+#define LIBENV "DYLD_INSERT_LIBRARIES"
+#define LIBPATH "../Resources/"
+#define LIBNAME "libQQUnrecall.dylib"
+
+#define HOOK "export " LIBENV "=%s" LIBPATH LIBNAME
+#define RUN "open -a QQ"
+
+#define CMD HOOK " && " RUN
+
+#define M 1024
+
+char path[M], buff[M];
+unsigned long len, i;
+
+int main(int argc, char **argv) {
+    strcpy(path, argv[0]);
+    len = strlen(path);
+    for (i = len - 1; i; i--) {
+        if (path[i] == '/')
+            break ;
+        path[i] = 0;
+    }
+    sprintf(buff, CMD, path);
+    system(buff);
+    return 0;
+}
